194cb369f15fadcdeb1d4f5a4f759744a24731bc
kkyeer <kkyeer@gmail.com>
2019-05-17 01:15:28 +0800

    Java并发编程：简单使用FutureTask模拟异步请求,传入function

--- src/main/java/concurrent/asynctask/TestCase.java
+++ src/main/java/concurrent/asynctask/TestCase.java
@@ -1,6 +1,10 @@
 package concurrent.asynctask;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 /**
  * @Author: kkyeer
@@ -11,6 +15,16 @@ import java.util.concurrent.ExecutionException;
 public class TestCase {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         WebDataLoader loader = new WebDataLoader();
+        Function<Map<String, Object>, Map<String, Object>> function = map -> {
+            AtomicInteger sum = new AtomicInteger(0);
+            map.values().forEach(
+                    val -> {sum.addAndGet((Integer) val);}
+            );
+            Map<String, Object> sumResult = new HashMap<>();
+            sumResult.put("sum", sum.get());
+            return sumResult;
+        };
         System.out.println(loader.getResult());
+        System.out.println(loader.getResult(function));
     }
 }
--- src/main/java/concurrent/asynctask/WebDataLoader.java
+++ src/main/java/concurrent/asynctask/WebDataLoader.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.function.Function;
 
 /**
  * @Author: kkyeer
@@ -18,13 +19,20 @@ class WebDataLoader {
         Thread.sleep(1000);
         System.out.println("Got result");
         Map<String, Object> result = new HashMap<>();
-        result.put("sum", 9999);
+        result.put("num1", 9999);
+        result.put("num2", 8888);
         return result;
     });
 
     public Map<String, Object> getResult() throws ExecutionException, InterruptedException {
         new Thread(futureTask).start();
         return futureTask.get();
+    }
+
+    public Map<String, Object> getResult(Function<Map<String,Object>,Map<String,Object>> function) throws ExecutionException, InterruptedException {
+        new Thread(futureTask).start();
+        Map<String, Object> result = futureTask.get();
+        return function.apply(result);
     }
 
 }
