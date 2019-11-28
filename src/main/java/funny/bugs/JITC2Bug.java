29260599cb17e39b933fef168925b5198a491279
kkyeer <kkyeer@gmail.com>
2019-11-28 19:23:57 +0800

    趣味知识：JIT BUG

--- src/main/java/funny/bugs/JITC2Bug.java
+++ src/main/java/funny/bugs/JITC2Bug.java
@@ -1,4 +1,4 @@
-package taste.bugs;
+package funny.bugs;
 
 /**
  * @Author: kkyeer
@@ -6,7 +6,7 @@ package taste.bugs;
  * @Date:Created in 10:02 11-3
  * @Modified By:
  */
-public class JVMC2Bug {
+public class JITC2Bug {
     public void test() {
         int i = 8;
         while ((i -= 3) > 0);
@@ -14,7 +14,7 @@ public class JVMC2Bug {
     }
 
     public static void main(String[] args) {
-        JVMC2Bug hello = new JVMC2Bug();
+        JITC2Bug hello = new JITC2Bug();
         for (int i = 0; i < 50_000; i++) {
             hello.test();
         }
