701110bbd232d85d12069c509c532e6473d6971e
kkyeer <kkyeer@gmail.com>
2020-06-03 17:21:21 +0800

    feat: netty echo server

---
+++ src/main/java/taste/io/netty/echoserver/EchoClient.java
@@ -0,0 +1,44 @@
+package taste.io.netty.echoserver;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @Author: kkyeer
+ * @Description: Echo客户端
+ * @Date:Created in 下午5:17 2020/6/3
+ * @Modified By:
+ */
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .remoteAddress(new InetSocketAddress(host, port))
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new EchoClientHandler());
+                    }
+                });
+        ChannelFuture channelFuture = bootstrap.connect().sync();
+        channelFuture.channel().closeFuture().sync();
+        eventLoopGroup.shutdownGracefully();
+    }
+}
---
+++ src/main/java/taste/io/netty/echoserver/EchoServer.java
@@ -0,0 +1,52 @@
+package taste.io.netty.echoserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @Author: kkyeer
+ * @Description: EchoServer
+ * @Date:Created in 下午4:24 2020/6/3
+ * @Modified By:
+ */
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new EchoServer(12345).start();
+    }
+
+    public void start() throws InterruptedException {
+        ChannelHandler channelHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(port)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(channelHandler);
+                    }
+                });
+        try {
+            ChannelFuture channelFuture = bootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+}
