5e0e467baf4ac671045f38a9175a2a75b34d619e
kkyeer <kkyeer@gmail.com>
2021-03-01 15:35:10 +0800

    leetcode: 1052

--- src/main/java/leetcode/editor/cn/GrumpyBookstoreOwner1052.java
+++ src/main/java/leetcode/editor/cn/GrumpyBookstoreOwner1052.java
@@ -100,28 +100,6 @@ class Solution {
         return sum + maxSuppressed;
 
     }
-
-
-        public int maxSatisfied2(int[] customers, int[] grumpy, int X) {
-            int total = 0;
-            int n = customers.length;
-            for (int i = 0; i < n; i++) {
-                if (grumpy[i] == 0) {
-                    total += customers[i];
-                }
-            }
-            int increase = 0;
-            for (int i = 0; i < X; i++) {
-                increase += customers[i] * grumpy[i];
-            }
-            int maxIncrease = increase;
-            for (int i = X; i < n; i++) {
-                increase = increase - customers[i - X] * grumpy[i - X] + customers[i] * grumpy[i];
-                maxIncrease = Math.max(maxIncrease, increase);
-            }
-            return total + maxIncrease;
-
-        }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
