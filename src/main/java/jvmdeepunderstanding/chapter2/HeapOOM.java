ce04084751ffd37b872b39448c559e27af8c4a43
kkyeer <kkyeer@gmail.com>
2021-03-09 14:30:55 +0800

    jvm: oom是否影响其他线程测试代码

--- src/main/java/jvmdeepunderstanding/chapter2/HeapOOM.java
+++ src/main/java/jvmdeepunderstanding/chapter2/HeapOOM.java
@@ -7,11 +7,28 @@ public class HeapOOM {
     static class  OOMObject{
         
     }
-//- Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
-    public static void main(String[] args) {
+// -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+    public static void main(String[] args) throws InterruptedException {
         List<OOMObject> list = new ArrayList<>();
-        while (true){
-            list.add(new OOMObject());
-        }
+        new Thread(
+                ()->{
+                    while (true){
+                        list.add(new OOMObject());
+                    }
+                }
+        ).start();
+        new Thread(
+                ()->{
+                    while (true){
+                        try {
+                            Thread.sleep(1000);
+                            System.out.println("哇哈哈");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).start();
+        Thread.sleep(900000000);
     }
 }
